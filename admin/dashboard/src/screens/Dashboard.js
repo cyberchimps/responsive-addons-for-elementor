00c7a66c14b25274ac678e8dcf146dcd24f124fd
Nikhil <sharma842nikhil@gmail.com>
2026-07-17 10:05:55 +0530

    remove new account button

--- admin/dashboard/src/screens/Dashboard.js
+++ admin/dashboard/src/screens/Dashboard.js
@@ -226,10 +226,7 @@ const ConnectWebsiteCard = () => {
                 </div>
 
                 <div className="flex flex-col gap-3">
-                    <button className="rst-start-auth rst-start-auth-new py-2.5 px-5 text-white leading-5 cursor-pointer connect-button rounded-md font-medium border-0">
-                        {__('Create a new account', 'responsive-addons-for-elementor')}
-                    </button>
-                    <button className="rst-start-auth rst-start-auth-exist py-2.5 px-5 text-[#1D4ED8] leading-5 cursor-pointer bg-white rounded-md font-medium border connect-exist connection-border">
+                    <button className="rst-start-auth rst-start-auth-exist py-2.5 px-5 text-white leading-5 cursor-pointer connect-button rounded-md font-medium border-0 connect-exist connection-border">
                         {__('Connect with existing account', 'responsive-addons-for-elementor')}
                     </button>
                 </div>
